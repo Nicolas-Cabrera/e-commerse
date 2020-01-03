c344be33bd10fadca3950af6035e26dbfbbc87a1
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-01-03 15:08:06 +0000

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import store from '../Images/shop.png';
+import cart from '../Images/cart.png';
+
 
 export default function Navbar() {
 
 	return (
-		<React.Fragment>
-			<h3>Hello from Navbar</h3>
-		</React.Fragment>
+		<nav className='navbar navbar-expands-sm bg-primary  navbar-dark px-sm-5'>
+			<Link to='/'>
+				<img src={store} alt='store' className='navbar-brand' />
+			</Link>					
+			<ul className='navbar-nav align-items-center'>
+				<li className='nav-item ml-5'>
+					<Link to='/' className='nav-link'>Products </Link>
+				</li>
+			</ul>
+			<Link to='/cart' className='ml-auto'>
+				<button>
+					<i className='fas fa-cart-plus' />
+					<img src={cart} alt='cart' />
+					My Cart
+				</button>
+			</Link>
+		</nav>
 	)
 }
